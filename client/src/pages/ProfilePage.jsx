221879b3b195616547d51457a14cf5912ebc1efa
rownok <rownok2001@gmail.com>
2023-04-02 22:34:26 +0600

    leftbar updated home

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import Leftbar from "../components/home/LeftBar";
 import UserProfile from "../components/profile/UserProfile";
 import RightBar from "../components/home/RightBar";
+import Navbar from "../components/home/Navbar";
 
 const ProfilePage = () => {
   return (
-
-      <div className="flex lg:gap-10 lg:px-48  mx-auto">
-        <Leftbar />
-        <UserProfile />
-        <RightBar />
-   
-    </div>
+    <div className="bg-[#f6fbff]">
+  
+    <Navbar/>
+   <div className="flex lg:px-48 mx-auto bg-[#f6fbff]">
+    <Leftbar />
+    <UserProfile/>
+    <RightBar />
+  </div>
+  </div>
   );
 };
 
