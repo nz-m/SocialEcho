f6ff59523cf4029c82ad7edf5b23a7bf3ba6cc07
rownok <rownok2001@gmail.com>
2023-04-15 10:07:42 +0600

    update modarator

--- client/src/components/moderator/ModeratorProfile.jsx
+++ client/src/components/moderator/ModeratorProfile.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getModProfileAction } from "../../redux/actions/authActions";
+import {HiOutlineUser} from "react-icons/hi2";
+import {MdOutlineMail,MdJoinInner} from "react-icons/md";
 
 const ModeratorProfile = () => {
   const dispatch = useDispatch();
@@ -13,23 +15,25 @@ const ModeratorProfile = () => {
   if (!moderator) return null; // later add a loading spinner
 
   return (
-    <div className="flex flex-col items-center justify-center rounded-lg shadow-md p-4">
+    <div className="flex flex-col gap-2 items-center justify-center bg-white sticky top-24 left-0 shadow-2xl shadow-[#F3F8FF] px-6 py-6 my-5 rounded-lg">
       <img
         src={moderator.avatar}
         alt="user"
         className="w-20 h-20 rounded-full object-cover"
       />
       <p>
-        <span className="font-bold">Moderator Profile</span>
+        <span className="font-bold">{moderator.name}</span>
       </p>
-      <p>
-        <span className="font-bold">Name:</span> {moderator.name}
+
+      <p className='flex items-center gap-2'>
+        <span className="font-bold">
+            <MdOutlineMail/>
+        </span> {moderator.email}
       </p>
-      <p>
-        <span className="font-bold">Email:</span> {moderator.email}
-      </p>
-      <p>
-        <span className="font-bold">Joined:</span> {moderator.createdAt}
+      <p className='flex items-center gap-2'>
+        <span className="font-bold">
+            <MdJoinInner/>
+        </span> {moderator.createdAt}
       </p>
       <p>Other infos here, will be added later</p>
     </div>
