8e9b8c4a7fcb9305272bd4290fba345500aff35c
Neaz Mahmud <neaz6160@gmail.com>
2023-05-31 04:25:53 +0600

    Update sign in & sign up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { signInAction } from "../redux/actions/authActions";
-import LoadingSpinner from "../components/loader/ButtonLoadingSpinner";
 import { AiFillGithub } from "react-icons/ai";
 import { MdOutlineAdminPanelSettings } from "react-icons/md";
+import ButtonLoadingSpinner from "../components/loader/ButtonLoadingSpinner";
 
 const SignIn = () => {
   const [loading, setLoading] = useState(false);
@@ -26,12 +26,16 @@ const SignIn = () => {
   const signInError = useSelector((state) => state.auth?.signInError);
   const successMessage = useSelector((state) => state.auth?.successMessage);
   return (
-    <section className="bg-[#F6F7FA]">
+    <section className="bg-white">
       <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
         <form className="w-full max-w-md">
-          <h1 className="mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl">
-            Sign In
-          </h1>
+          <div className="flex justify-center mx-auto">
+            <img
+              className="w-auto h-7 sm:h-8"
+              src="https://merakiui.com/images/logo.svg"
+              alt=""
+            />
+          </div>
           {signInError && (
             <div
               className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mt-6"
@@ -50,11 +54,26 @@ const SignIn = () => {
               <span className="block sm:inline">{successMessage}</span>
             </div>
           )}
-          <div className="relative flex items-center mt-8">
+          <div className="flex items-center justify-center mt-6">
+            <Link
+              to={"/signin"}
+              className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 text-gray-300"
+            >
+              sign in
+            </Link>
+            <Link
+              to={"/signup"}
+              className="w-1/3 pb-4 font-medium text-center text-gray-500 capitalize border-b border-gray-400 text-gray-300"
+            >
+              sign up
+            </Link>
+          </div>
+
+          <div className="relative flex items-center mt-6">
             <span className="absolute">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                className="w-6 h-6 mx-3 text-gray-300"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -68,19 +87,21 @@ const SignIn = () => {
               </svg>
             </span>
             <input
+              id="email"
+              name="email"
+              type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required
-              type="email"
-              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11   focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               placeholder="Email address"
+              required
             />
           </div>
           <div className="relative flex items-center mt-4">
             <span className="absolute">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                className="w-6 h-6 mx-3 text-gray-300"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -94,35 +115,30 @@ const SignIn = () => {
               </svg>
             </span>
             <input
+              id="password"
+              name="password"
+              type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              required
-              type="password"
-              className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg   focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               placeholder="Password"
+              required
             />
           </div>
           <div className="mt-6">
             <button
               disabled={loading}
               onClick={handleSubmit}
-              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary rounded-lg hover:bg-primary-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+              className={`w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50 ${
+                loading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
             >
               {loading ? (
-                <LoadingSpinner loadingText={"Signing in..."} />
+                <ButtonLoadingSpinner loadingText={"Signing in..."} />
               ) : (
                 "Sign in"
               )}
             </button>
-
-            <div className="mt-6 text-center ">
-              <Link
-                to="/signup"
-                className="text-sm text-blue-500 hover:underline dark:text-blue-400"
-              >
-                Don't have an account? Sign up
-              </Link>
-            </div>
           </div>
         </form>
       </div>
