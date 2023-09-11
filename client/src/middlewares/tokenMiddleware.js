4dc6c7822bfff5c0b13e1b8098e8d7ae0084939a
Neaz Mahmud <neaz6160@gmail.com>
2023-09-11 20:59:06 +0600

    Update tokenMiddleware.js

--- client/src/middlewares/tokenMiddleware.js
+++ client/src/middlewares/tokenMiddleware.js
@@ -1,33 +1,25 @@
 import jwt_decode from "jwt-decode";
 import { refreshTokenAction } from "../redux/actions/refreshTokenAction";
 
-const TOKEN_REFRESH_THRESHOLD = 1800000; // 30 minutes in ms
-
-const shouldRefreshToken = (token) => {
-  if (!token) return false;
-
-  const expiresIn = jwt_decode(token).exp * 1000 - Date.now();
-  return expiresIn < TOKEN_REFRESH_THRESHOLD;
-};
-
 export const tokenMiddleware = (store) => (next) => async (action) => {
   if (action.meta && action.meta.requiresAuth) {
-    const state = store?.getState();
+    const state = store.getState();
     const token = state.auth?.accessToken;
-
-    if (shouldRefreshToken(token)) {
-      const refreshToken = state.auth.refreshToken;
-      try {
-        await store.dispatch(refreshTokenAction(refreshToken));
-        const newToken = store?.getState().auth.accessToken;
-
-        if (!newToken) {
-          throw new Error("Access token not found after refresh");
+    if (token) {
+      const expiresIn = jwt_decode(token).exp * 1000 - Date.now();
+      if (expiresIn < 1800000) {
+        const refreshToken = state.auth.refreshToken;
+        try {
+          await store.dispatch(refreshTokenAction(refreshToken));
+          const newToken = store.getState().auth.accessToken;
+          if (!newToken) {
+            throw new Error("Access token not found after refresh");
+          }
+        } catch (error) {
+          store.dispatch({ type: "LOGOUT" });
         }
-      } catch (error) {
-        store.dispatch({ type: "LOGOUT" });
       }
-    } else if (!token) {
+    } else {
       store.dispatch({ type: "LOGOUT" });
     }
   }
